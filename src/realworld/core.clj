81c9e0bc274d23e61a1b2fc23d17e0c94e2859c7
Hoon Wee <mrgravity817@gmail.com>
2024-05-20 14:16:00 +0900

    feat: setup pedestal

--- src/realworld/core.clj
+++ src/realworld/core.clj
@@ -1,6 +1,30 @@
-(ns realworld.core)
+(ns realworld.core
+  (:require [realworld.config :as config]
+            [io.pedestal.http.route :as route]
+            [io.pedestal.http :as http]))
+
+(defn respond-hello [request]
+  {:status 200
+   :body   "Hello, World!"})
+
+(def routes
+  (route/expand-routes
+   #{["/greet" :get respond-hello :route-name :greet]}))
+
+(defn create-server []
+  (http/create-server
+   {::http/routes routes
+    ::http/type   :jetty
+    ::http/port   8080}))
+
+(defn start-server []
+  (http/start (create-server)))
+
+(start-server)
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (let [config (config/read-config)]
+    (println "Starting RealWorld with config:" config)
+    (start-server)))
